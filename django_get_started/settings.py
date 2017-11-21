3a8f91ea0ca6dd68e9bb18d21ad6dfa0c3d42249
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-21 21:20:45 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -11,6 +11,8 @@ mysqlconndict = dict(s.split('=',1) for s in mysqlconnlst)
 
 PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 DEBUG = True
