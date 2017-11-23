bbb318c5c38cb9a7e3019c90e7d2fe3c88cc72ac
EvertonAlauk <30833016+EvertonAlauk@users.noreply.github.com>
2017-11-23 19:07:29 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -103,8 +103,8 @@ class Professor(Usuario):
       
 class Turma(models.Model):
       turma = models.CharField(max_length=15)
+      disciplina = models.ForeignKey(Disciplina)
       disciplinaOfertada = models.ForeignKey(DisciplinaOfertada)
-      disciplinaOfertada = models.ForeignKey(Disciplina)
       professor = models.ForeignKey(Professor)
       def _str_(self):
             return self.turma
