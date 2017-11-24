2bd5b439f5880a63c8df8bd8d5e9d79f9bc9e9a6
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-24 11:52:10 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-import datetime
 # Create your models here.
 
 class UsuarioManager(BaseUserManager):
@@ -122,9 +121,9 @@ class CursoTurma(models.Model):
       
 class Questao(models.Model):
       numero = models.IntegerField(null=True)
-      data_limite_entrega = models.DateField(DATE)
+      data_limite_entrega = models.DateField(auto_now=False, auto_now_add=False)
       descricao = models.TextField(null=True)
-      data = models.DateField(DATE)
+      data = models.DateField(auto_now=False, auto_now_add=False)
       turma = models.ForeignKey(Turma)
       def _str_(self):
             return self.numero
@@ -139,11 +138,11 @@ class ArquivosQuestao(models.Model):
 class Resposta(models.Model):
       questao = models.ForeignKey(Questao)
       aluno = models.ForeignKey(Aluno)
-      data_avaliacao = models.DateField(DATE)
+      data_avaliacao = models.DateField(auto_now=False, auto_now_add=False)
       nota = models.DecimalField(max_digits=4, decimal_places=2)
       avaliacao = models.TextField(null=True)
       descricao = models.TextField(null=True)
-      data_de_envio = models.DateField(DATE)
+      data_de_envio = models.DateField(auto_now=False, auto_now_add=False)
       def _str_(self):
             return self.avaliacao
       
