17e980275cc2652430cda778c8dad42c4cbd9434
EvertonAlauk <30833016+EvertonAlauk@users.noreply.github.com>
2017-11-26 21:19:50 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -1,15 +1,13 @@
+
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from app.views import *
 from django.views.generic import RedirectView
 from django.contrib.auth.views import login,logout
-
+from app.views import *
 
 urlpatterns = [
-
     url(r'^$', login,{ 'template_name':'index.html' }),
-   # url(r'^index.html', index),
     url(r'^contato.html', login,{ 'template_name':'contato.html' }),
     url(r'^logado.html', login,{ 'template_name':'logado.html' }),
     url(r'^disciplinas.html', login,{ 'template_name':'disciplinas.html' }),
@@ -19,6 +17,5 @@ urlpatterns = [
     url(r'^eventos.html', login,{ 'template_name':'eventos.html' }),
     url(r'^blog.html', login,{ 'template_name':'blog.html' }),
     url(r'^index.html', login,{ 'template_name':'index.html' }),
-    url(r'^sair/', logout , {'next_page': '/index.html'})
-    
+    url(r'^sair/', logout , {'next_page': '/index.html'}) 
 ]
