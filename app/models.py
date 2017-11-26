8c7cf746b4b0edcdd69759741c07584fc0c9ac82
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-26 17:17:29 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,54 +7,53 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 class UsuarioManager(BaseUserManager):
-    
-    use_in_migrations = True
-    def _create_user(self, ra, password, **extra_fields):
-       	if not ra:
-            raise ValueError('RA precisa ser preenchido')
-        user = self.model(ra=ra, **extra_fields)
-       	user.set_password(password)
-       	user.save(using=self._db)
-       	return user
+      use_in_migrations = True
+      def _create_user(self, ra, password, **extra_fields):
+          if not ra:
+              raise ValueError('RA precisa ser preenchido')
+          user = self.model(ra=ra, **extra_fields)
+          user.set_password(password)
+          user.save(using=self._db)
+          return user
 
-    def create_user(self, ra, password=None, **extra_fields):
-        return self._create_user(ra, password, **extra_fields)
+      def create_user(self, ra, password=None, **extra_fields):
+          return self._create_user(ra, password, **extra_fields)
 
-    def create_superuser(self, ra, password, **extra_fields):
-        return self._create_user(ra, password, **extra_fields)
+      def create_superuser(self, ra, password, **extra_fields):
+          return self._create_user(ra, password, **extra_fields)
 
 
 class Usuario(AbstractBaseUser):
-    nome = models.CharField(max_length=50)
-    ra = models.IntegerField(unique=True)
-    email = models.CharField(max_length=150)
-    celular = models.CharField(max_length=150)
-    perfil = models.CharField(max_length=1,default='C')
-    ativo = models.BooleanField(default=True)
+      nome = models.CharField(max_length=50)
+      ra = models.IntegerField(unique=True)
+      perfil = models.CharField(max_length=1, default='C')
+      ativo = models.BooleanField(default=True)
+      email = models.CharField(max_length=80)
+      celular = models.CharField(max_length=11)
 
-    USERNAME_FIELD = 'ra'
-    REQUIRED_FIELDS = ['nome']
+      USERNAME_FIELD = 'ra'
+      REQUIRED_FIELDS = ['nome']
 
-    objects = UsuarioManager()
+      objects = UsuarioManager()
 
-    @property
-    def is_staff(self):
-	return self.perfil == 'C'
+      @property
+      def is_staff(self):
+            return self.perfil == 'C'
 
-    def has_perm(self, perm, obj=None):
-	return True
+      def has_perm(self, perm, obj=None):
+            return True
+      
+      def has_module_perms(self, app_label):
+            return True
+  
+      def get_short_name(self):
+            return self.nome
+      
+      def get_full_name(self):
+            return self.nome
 
-    def has_module_perms(self, app_label):
-	return True
-
-    def get_short_name(self):
-	return self.nome
-
-    def get_full_name(self):
-	return self.nome
-
-    def __str__(self):
-        return self.nome
+      def _str_(self):
+            return self.nome
 
 class Curso(models.Model):
     sigla = models.CharField(primary_key=True,max_length=5)
