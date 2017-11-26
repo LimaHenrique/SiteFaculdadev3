be7fcc91e0606e2203d634bcdb44e98464a531b5
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-26 11:48:17 -0200

    Add files via upload

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -1,18 +1,3 @@
-"""siteFaculdade URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
@@ -30,7 +15,6 @@ urlpatterns = [
     url(r'^disciplinas.html', login,{ 'template_name':'disciplinas.html' }),
     url(r'^inscricao.html', login,{ 'template_name':'inscricao.html' }),
     url(r'^admin/', admin.site.urls),
-   #url(r'^admin', admin.site.urls),
     url(r'^cursos.html', login,{ 'template_name':'cursos.html' }),   
     url(r'^eventos.html', login,{ 'template_name':'eventos.html' }),
     url(r'^blog.html', login,{ 'template_name':'blog.html' }),
