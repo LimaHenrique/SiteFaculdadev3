42394c1be0eafb38f9e0790551c34067ebdaa2f8
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-27 15:03:27 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -106,7 +106,7 @@ LOGIN_REDIRECT_URL= 'logado.html'
 
 EMAIL_BACKEND = "django.app.mail.backends.smtp.Emailbackend"
 EMAIL_USER_TLS = True
-EMAIL_HOST = "smtp.hotmail.com.com"
-EMAIL_HOST_USER = "henrique_lindao1999@hotmail.com"
-EMAIL_HOST_PASSWORD = "Henrique 0303"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "evertonalaauk@gmail.com"
+EMAIL_HOST_PASSWORD = "kaonbolado001002036"
 EMAIL_PORT = 587
