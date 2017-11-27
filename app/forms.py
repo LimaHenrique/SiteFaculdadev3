d191fa8308068ad4ae96dac1fb530a2ed8c13b6c
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-27 00:01:00 -0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,24 @@
 from django import forms
+from app.models import Questao
+from django.core.mail import send_mail
+from siteFaculdade.settings import *
 
-class Contato(forms.Form):
-    assunto = forms.CharField(label="Assunto", required = True)
-    nome = forms.CharField(label="Nome",required = True )
-    email = forms.EmailField(label="E-mail", help_text="Informe um E-mail válido")
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label="nome", required=True )
+    email = forms.EmailField(label="email", help_text="Informe um e-mail válido")
     telefone = forms.NumberInput()
-    mensagem = forms.CharField(label = "Mensagem" , required = "true")
+    ra = forms.NumberInput()
+    assunto = forms.CharField(label="assunto", required=True)
+    mensagem = forms.CharField(label="mensagem", required="true")
+
+    def mandar_email(self, assunto, mensagem, email_enviar):
+        print("Agora deu certo.")
+        emailOrigem = EMAIL_HOST_USER
+        emailDestino = [email_enviar]
+        send_mail(assunto, mensagem, emailOrigem, emailDestino, fail_silently=True)
+    
+class QuestaoForm(forms.ModelForm):
+    class Meta:
+        model = Questao
+        fields = "__all__"
+
