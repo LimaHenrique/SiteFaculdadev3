db463e33082c0e14f9fb622b65467ef805ada4cf
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-27 02:48:45 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -49,10 +49,9 @@ def disciplinas(request):
     return render(request, "disciplinas.html" , context)	
 	
 def restrito(request):
+	
     context = { 
-	    
-	   "cursos" : Curso.objects.all()
-	    
+	    "cursos" : Curso.objects.all()
     }
     return render(request, "restrito.html", contexto)
 
