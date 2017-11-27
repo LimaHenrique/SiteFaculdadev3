573c7bc9d8818d00914e70fff0f13523240098ff
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-27 17:00:14 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^index.html', login,{ 'template_name':'index.html' }),
     url(r'^questao_form.html', login,{ 'template_name':'questao_form.html'}, name="questao_form" ),
     url(r'^restrito/(?P<sigla>[A-Z,a-z]+)/questao/(?P<questao_id>[0-9]*)', questao_form, name="questao_form"),
-    url(r'^cursos.html/([A-Z,a-z]+)', cursos) 
+    url(r'^cursos.html/([A-Z,a-z]+)', cursos),
     url(r'^restrito.html', restrito, name="restrito"),
     url(r'^sair/', logout , {'next_page': '/index.html'}), 
     
