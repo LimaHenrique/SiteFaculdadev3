8f071063a9b0b4c5e8ebc392b86ccb763c50291e
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-27 01:38:57 -0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -14,15 +14,7 @@ class ContatoForm(forms.Form):
     nome = forms.CharField()
     email = forms.EmailField()
     mensagem = forms.CharField()
-
-    def envia_email(self):
-        print(
-            "Email Para você:\n"+
-            "Aluno: "+self.cleaned_data["nome"]+"\n"+
-            "Email: "+self.cleaned_data["email"]+"\n"+
-            "Mensagem: "+self.cleaned_data["mensagem"]
-        )
-
+    
 class QuestaoForm(forms.ModelForm):
 
     class Meta:
