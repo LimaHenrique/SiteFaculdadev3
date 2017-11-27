06a4b26d2b71e189e904b6fdc860dae7391cdfca
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-27 00:25:27 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -96,6 +96,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+MEDIA_URL  = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 #STATIC_URL = 'app/static/'
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'app.Usuario'
