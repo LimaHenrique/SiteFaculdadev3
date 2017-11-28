9a379afb630cbeb2e75ee707e5bad413aa69e9d4
EvertonAlauk <30833016+EvertonAlauk@users.noreply.github.com>
2017-11-28 20:51:41 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 from django.contrib.auth.views import login,logout
 from django.conf import settings
 from django.conf.urls.static import static
-from app.views import index,contato,cursos,inscricao,logado,disciplinas,restrito
+from app.views import *
 
 urlpatterns = [
     url(r'^$', login,{ 'template_name':'index.html' }),
